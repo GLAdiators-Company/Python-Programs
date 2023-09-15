d8edbb3d024905f11d47a17e323a4e4c5066a3ae
Yash Agrawal <yashagrawal9319029562@gmail.com>
2023-09-16 00:08:39 +0530

    shutil_module_added

---
+++ shutil_module.py
@@ -0,0 +1,21 @@
+import shutil
+import os
+# shutil.copy("time_module.py","hello.txt")    # The copy methods copyt the content of a file into another
+# os.mkdir('Hello')
+# with open('Hello\\hello.txt','w') as file:
+#     for i in range(10):
+#         file.write('This is a file\n')
+
+# shutil.copytree('Hello','TimePass')   #The copytree method copy the content of a folder into another
+
+
+# shutil.move('Hello\\class_methods.py','class_methods.py') # This method move file from one destination to another
+
+# shutil.rmtree('Hello')  #This method is used to delete a folder
+
+
+
+
+
+
+
