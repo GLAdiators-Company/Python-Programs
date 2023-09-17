53d278c87c060546610e2c174cd667e580aa2dce
Yash Agrawal <yashagrawal9319029562@gmail.com>
2023-09-17 13:44:44 +0530

    edited

--- shutil_module.py
+++ shutil_module.py
@@ -1,17 +1,17 @@
 import shutil
 import os
-# shutil.copy("time_module.py","hello.txt")    # The copy methods copyt the content of a file into another
-# os.mkdir('Hello')
-# with open('Hello\\hello.txt','w') as file:
-#     for i in range(10):
-#         file.write('This is a file\n')
+shutil.copy("time_module.py","hello.txt")    # The copy methods copyt the content of a file into another
+os.mkdir('Hello')
+with open('Hello\\hello.txt','w') as file:
+    for i in range(10):
+        file.write('This is a file\n')
 
-# shutil.copytree('Hello','TimePass')   #The copytree method copy the content of a folder into another
+shutil.copytree('Hello','TimePass')   #The copytree method copy the content of a folder into another
 
 
-# shutil.move('Hello\\class_methods.py','class_methods.py') # This method move file from one destination to another
+shutil.move('Hello\\class_methods.py','class_methods.py') # This method move file from one destination to another
 
-# shutil.rmtree('Hello')  #This method is used to delete a folder
+shutil.rmtree('Hello')  #This method is used to delete a folder
 
 
 
